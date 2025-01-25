4f6ed2cf7cedf04a6d288edd5e76948ca85f8024
Carolyn Aquino <caquino1223@gmail.com>
2025-01-25 11:19:09 -0500

    updating css

--- src/components/Clipboard.js
+++ src/components/Clipboard.js
@@ -5,6 +5,7 @@ import ClipboardImage from "../assets/clipboard.svg";
 function Clipboard({ title, content }) {
   return (
     <div className="clipboard">
+    <img src={ClipboardImage} alt="Clipboard SVG" className="clipboard-image" />
       <h2 className="clipboard-title">{title}</h2>
       <p className="clipboard-paragraph">{content}</p>
     </div>
