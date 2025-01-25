950184d66e24bfa19e944e3429ba195c0d55f47d
Carolyn Aquino <caquino1223@gmail.com>
2025-01-25 14:42:25 -0500

    additional formatting changes

--- src/components/Clipboard.js
+++ src/components/Clipboard.js
@@ -5,7 +5,11 @@ import ClipboardImage from "../assets/clipboard.svg";
 function Clipboard({ title, content }) {
   return (
     <div className="clipboard">
-    <img src={ClipboardImage} alt="Clipboard SVG" className="clipboard-image" />
+      <img
+        src={ClipboardImage}
+        alt="Clipboard SVG"
+        className="clipboard-image"
+      />
       <h2 className="clipboard-title">{title}</h2>
       <p className="clipboard-paragraph">{content}</p>
     </div>
